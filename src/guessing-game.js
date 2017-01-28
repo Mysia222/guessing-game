9e2637b4f317b96b553dfc9bfb0d28d71fb19be5
Yulia <milkiway897@gmail.com>
2017-01-28 18:54:30 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,6 +1,5 @@
 class GuessingGame {
     constructor() {
-    
     this.max1=0;
     this.min1=0;
     this.number=0
@@ -11,22 +10,17 @@ class GuessingGame {
     	    };
 
     this.guess = function (){
-
       this.number = Math.ceil((this.min1 + this.max) / 2)+1;
       return this.number;
     };
 
     this.lower= function (){
-    	this.max1=this.number-1;
-		
+    	this.max1=this.number-1;	
     };
 
     this.greater=function (){
-   	 	this.min1=this.number;
-		
+   	 	this.min1=this.number;	
     };
   } 
 }
 module.exports = GuessingGame;
-
-
