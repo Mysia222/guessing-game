3014cebf1e05de7426b7c9cc90f72ea73997f713
Mysia22 <milkiway897@gmail.com>
2017-01-28 16:46:22 +0300

    alll

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,15 +2,17 @@ class GuessingGame {
     constructor() {
    this.min=0;
    this.max=0;
-   this.number=0;
+   //this.number=0;
 
     this.setRange = function (min, max) {
     	
+    	this.min=min;
+    	this.max=max;
     	
     };
 
     this.guess = function (){
-    	var max,min,k;
+   /* 	var max,min,k;
     	this.setRange(max,min);
     	 while (min < max)                
        {  k = Math.floor((min+max)/2);
@@ -21,19 +23,19 @@ class GuessingGame {
        if(min==number)
        	return min;
        else
-       	return -1;
+       	return -1;*/
     };
 
     this.lower= function (){
-var min,max;
-this.setRange(min,max);
-min=this.k;
+//var min,max;
+//this.setRange(min,max);
+//min=this.k;
     };
 
     this.greater=function (){
-var min,max;
-this.setRange(min,max);
-min=this.k+1;
+//var min,max;
+//this.setRange(min,max);
+//min=this.k+1;
     };
 }
 }
